1e6ab20c28e3ad1a923b767176452ef06446243e
alexkobus <70383566+alexkobus@users.noreply.github.com>
2022-09-30 18:56:09 -0400

    Setup SpringBoot & Fix test URL & Add param error handling to test page

--- backend/src/main/java/com/purduecoursefinder/controllers/TestController.java
+++ backend/src/main/java/com/purduecoursefinder/controllers/TestController.java
@@ -8,8 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/test")
 public class TestController {
-	@GetMapping(value = "/")
-	public String index(@RequestParam(name = "number", required = false) Integer number) {
-		return "It works! Param: " + (number == null ? "" : number);
+	@GetMapping()
+	public String index(@RequestParam(name = "number", required = false) String number) {
+		int num;
+		if (number == null) {
+			return "It works!";
+		}
+
+		try {
+			num = Integer.parseInt(number);
+			return "It works! Param: " + num;
+		} catch (Exception e) {
+			return "Error: Provided parameter is not an integer";
+		}
 	}
 }
