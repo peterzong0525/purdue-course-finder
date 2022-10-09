de8cf70c974384b0c73609eb095c646ccef13901
peterzong0525 <70426664+peterzong0525@users.noreply.github.com>
2022-10-08 21:55:14 -0400

    Updated structure, improved testing navigation ability

---
+++ frontend/purdue-course-finder/src/components/tutorialPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Button} from "react-bootstrap"
+import './tutorialPage.css'
+
+function tutorialPage() {
+    return(
+        <div>
+            <h1>Tutorial Page</h1>
+            <a href='/'>Home</a>
+        </div>
+    );
+}
+
+export default tutorialPage;
