b2b0db7c665edf76f42300798c07b16761a31da2
peterzong0525 <70426664+peterzong0525@users.noreply.github.com>
2022-10-09 22:29:38 -0400

    Added UI Mockups from the Design Document and described how to use them

--- frontend/purdue-course-finder/src/components/tutorialPage.js
+++ frontend/purdue-course-finder/src/components/tutorialPage.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import './tutorialPage.css'
 
+import signupImg from '../design_images/signup.png'
+import loginImg from '../design_images/login page.PNG'
+import favoriteImg from '../design_images/favoriting.PNG'
+import homeImg from '../design_images/home page.PNG'
+import homeBuildingImg from '../design_images/home page building selection.PNG'
+import homeCourseImg from '../design_images/home page course selection.PNG'
+import courseScheduleImg from '../design_images/course schedule page.PNG'
+
 function tutorialPage() {
     return(
         <div className='tutorialPageContainer'>
@@ -80,22 +88,51 @@ function tutorialPage() {
                     <p>Text Here</p>
                     <div className='subSection'>
                         <h3 id='Signing_Up'>Signing Up</h3>
-                        <p>Text Here</p>
+                        <img src={signupImg} alt=''></img>
+                        <p>
+                            If you do not already have an account, you may create one on the <a href='./signup'>signup</a> page. 
+                            Please enter your email address, password, and re-enter your password to create a new account.
+                            If you recieve an error upon hitting the Sign Up button, your password either does not match
+                            the required format or the email you have entered is already registered to an account. To log in
+                            instead, please click the log in link at the bottom of the form, bringing you to 
+                            the <a href='./login'>login</a> page.
+                        </p>
                         <h3 id='Logging_In'>Logging In</h3>
-                        <p>Text Here</p>
+                        <img src={loginImg} alt=''></img>
+                        <p>
+                            To log in, please enter your email address and password and hit the Log In button. 
+                            If you receive an error, the email address or password you entered is incorrect. 
+                            If you would like to reset your password, please click the <a href='./'>Forgot Password?</a> link. 
+                            If you would like to create an account, please click the <a href='./signup'>Create an Account</a> link.
+                        </p>
                         <h3 id='Modifying_Account_Info'>Modifying Account Info</h3>
-                        <p>Text Here</p>
+                        <img  alt='No image yet'></img>
+                        <p>
+                            If for any reason you would like to change your account email address or password, you can do so on this page.
+                            More details to come...
+                        </p>
                     </div>
                 </div>
                 <div id='Favoriting_System'>
                     <hr></hr>
                     <h2>Favoriting System</h2>
-                    <p>Text Here</p>
+                    <img src = {favoriteImg} alt=''></img>
+                    <p>
+                        If you are logged in, you may favorite classes, buildings, rooms and sections. To favorite,
+                        click on the Star next to the item of interest in the sidebar. To manage your favorites,
+                        please go to the <a href='./'>favorites</a> page.
+                    </p>
                 </div>
                 <div id='Personal_Shedules'>
                     <hr></hr>
                     <h2>Personal Schedules</h2>
-                    <p>Text Here</p>
+                    <img src={courseScheduleImg} alt=''></img>
+                    <p>
+                        If you are logged in, you can create a personal schedule displaying a weekly schedule of your courses.
+                        To add a new course, Something will happen, more deatils to come... To view more details for a course
+                        on the schedule, highlight over the course on the calendar. In the event that two courses overlap,
+                        the calendar will split the courses evenly in the space available.
+                    </p>
                 </div>
                 <div id='Navigation'>
                     <hr></hr>
@@ -103,9 +140,30 @@ function tutorialPage() {
                     <p>Text Here</p>
                     <div className='subSection'>
                         <h3 id='Map'>Map</h3>
-                        <p>Text Here</p>
+                        <img src={homeImg} alt='No image yet'></img>
+                        <p>
+                            To move around the map, simply drag along the map. All buildings currently registered in the system
+                            will be clickable on the map.
+                        </p>
+                        <img src={homeBuildingImg} alt='No image yet'></img>
+                        <p>
+                            When you click on a building on a map, relevant information will appear
+                            in the sidebar (and maybe a popup (TBD)).
+                        </p>
                         <h3 id='Sidebar'>Sidebar</h3>
-                        <p>Text Here</p>
+                        <img src={homeCourseImg} alt='No image yet'></img>
+                        <p>
+                            The sidebar, by default, contains all the buildings registered in the system. 
+                            To search for a course, building, room, or section, you can use the search bar located at
+                            the top of the sidebar.
+                        </p>
+                        <p>
+                            In the event that the course, building, room, or section you searched for is not found, 
+                            you can manually search for it under the advanced search (not yet in the UI).
+                            Fill out as many sections as you can and hit the search button to call the Purdue API. If
+                            not results are returned in the sidebar, the course, building, room, or section you are
+                            searching for is not currently registered in the Purdue system.
+                        </p>
                     </div>    
                 </div>
             </div>
