4ee27ae22069269423f5b294e7dfea5f39889867
alexkobus <70383566+alexkobus@users.noreply.github.com>
2022-10-12 18:22:59 -0400

    update Courses.js to get correct data from server updates & add https-start script

--- frontend/purdue-course-finder/src/components/Courses.js
+++ frontend/purdue-course-finder/src/components/Courses.js
@@ -15,6 +15,7 @@ function Courses() {
         axios.get(url).then((response) => {
             const data = response.data;
             setCourses(data);
+            console.log(data);
         });
     }, [])
 
@@ -25,12 +26,12 @@ function Courses() {
 
     return courses.map((course, index) => (
         <div key={index}>
-            <p>ID: {course.id}</p>
-            <p>Name: {course.subject.abbreviation} {course.courseNumber}</p>
-            <p>Title: {course.title}</p>
-            <p>Credits: {course.creditHours}</p>
-            <p>Description: {course.description}</p>
-            <a href={`../sections/${course.id}`}>Sections</a>
+            <p>ID: {course.classId}</p>
+            <p>Name: {course.course.subject.abbreviation} {course.course.courseNumber}</p>
+            <p>Title: {course.course.title}</p>
+            <p>Credits: {course.course.creditHours}</p>
+            <p>Description: {course.course.description}</p>
+            <a href={`../sections/${course.classId}`}>Sections</a>
             <p>-----------</p>
         </div>
     ))
