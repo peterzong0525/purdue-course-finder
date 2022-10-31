a346c51ed559c8ef23ffa14b9098f7ee6613aca1
thomas-lane <70451454+thomas-lane@users.noreply.github.com>
2022-10-30 21:45:10 -0400

    Added tests for controllers

--- backend/src/test/java/com/purduecoursefinder/PurdueCourseFinderApplicationTests.java
+++ backend/src/test/java/com/purduecoursefinder/PurdueCourseFinderApplicationTests.java
@@ -1,13 +1,13 @@
 package com.purduecoursefinder;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PurdueCourseFinderApplicationTests {
+public class PurdueCourseFinderApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
