65437101554d27a8a5d709a5c1db5bb5ec96e6c6
peterzong0525 <70426664+peterzong0525@users.noreply.github.com>
2022-11-08 23:56:42 -0500

    Added partial infrastructure to determine event locations

--- frontend/purdue-course-finder/src/components/Schedule.js
+++ frontend/purdue-course-finder/src/components/Schedule.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
+import axios from 'axios';
+import { serverURL } from '../index.js';
 import './Schedule.css'
 
 function Schedule() {
@@ -19,6 +21,111 @@ function Schedule() {
             return;
         }
     }, [])
+
+    function addAllScheduleEvents() {
+
+    }
+
+    function addOneCourse() {
+
+    }
+
+    function addOneSection(/*sectionData*/dayOfWeek, startTime, durationLength, sectionType) {
+        let sectionMeetings = [];
+        let daysOfWeek = ['Sunday', 'Tuesday', 'Wednesday', 'Saturday']/*sectionData.Meetings.DaysOfWeek.split(", ")*/;
+        daysOfWeek.forEach(day => {
+            sectionMeetings.push(addOneMeeting(day, startTime, durationLength, sectionType));
+        });
+        let className = "meeting-container " + dayOfWeek//" section-id"
+
+        function setSpecial(className, enterOrExit /*true on enter, false on exit */) {
+            let elements = document.getElementsByClassName(className)
+            if (enterOrExit) {
+                for (let i = 0; i < elements.length; i++) {
+                    elements[i].setAttribute('selected', '1');
+                }
+            } else {
+                for (let i = 0; i < elements.length; i++) {
+                    elements[i].removeAttribute('selected');
+                }
+            }
+
+        }
+        return sectionMeetings.map((meeting, index) => (
+            <div className={className} key={index} style={{position: "absolute"}}
+                onMouseEnter={()=>{setSpecial(className, true)}}
+                onMouseLeave={()=>{setSpecial(className, false)}}>
+                {meeting}
+            </div>
+        ))
+    }
+    
+
+    function addOneMeeting(dayOfWeek, startTime, durationLength, sectionType) {     
+        /*  Expected formats:  
+              dayOfWeek: Sunday/Monday/Tuesday...
+              startTime: 2022-11-04T15:30:00.000+00:00
+              durationLength: PT1H50M
+              sectionType: Lecture, Practice Study Observation, Distance Learning...
+        */
+
+        const baseLeftLoc = "51.5";
+        const map_DofW_to_number = {
+            "Sunday": 0,
+            "Monday": 1,
+            "Tuesday": 2,
+            "Wednesday": 3,
+            "Thursday": 4,
+            "Friday": 5,
+            "Saturday": 6
+        }
+        let posFromLeft = "calc(" + baseLeftLoc + "px + " + map_DofW_to_number[dayOfWeek] + " * ((100vw - 67px) / 7))";
+
+        const baseTopLoc = 27.5;
+        let startTimeDate = new Date(startTime);
+        let startHour = startTimeDate.getUTCHours();
+        let startMinute = startTimeDate.getUTCMinutes();
+        let posFromTop = baseTopLoc + (((startHour * 61) + startMinute) * 2);
+
+        let durationObject = durationLength.substring(2);
+        let durationHours = 0;
+        if (durationObject.indexOf('H') !== -1) {
+            durationHours = durationObject.substring(0, durationObject.indexOf('H'));
+            durationObject = durationObject.substring(durationObject.indexOf('H') + 1);
+        }
+        let durationMinutes = 0;
+        if (durationObject.indexOf('M') !== -1) {
+            durationMinutes = durationObject.substring(0, durationObject.indexOf('M'));
+        }
+        durationMinutes = parseInt(durationMinutes) + parseInt(durationHours * 60);
+        let height = durationMinutes * 2;
+
+        // Temporary random colors
+        const map_section_type_to_color = {
+            "Distance Learning": "#FFFFFF",
+            "Experiential": "#BBBBBB",
+            "Individual Study": "#123123",
+            "Laboratory": "#321312",
+            "Lecture": "#1CD2FF",
+            "Practice Study Observation": "#000000",
+            "Recitation": "#A40101"
+        }
+
+        let styleString = {
+            transform: "translate(" + posFromLeft + ", " + posFromTop + "px)",
+            height: height + "px",
+            backgroundColor: map_section_type_to_color[sectionType]
+        }
+
+        return (
+            <div className="schedule-event" style={styleString}>
+                Details
+            </div>
+        )
+    }
+
+    
+
 
     const generateGrids = () => {
         let grids = [<div key="none"/>];
@@ -52,29 +159,29 @@ function Schedule() {
     }
 
 
-    // On page load, scroll to scpeific starting time
+    // On page load, scroll to specific starting time
     window.addEventListener('load', () => {
-        let scrollElement = document.querySelector('.scheduleContainer');
-        scrollElement.scrollTop = 700;
+        let scrollElement = document.querySelector('.schedule-container');
+        scrollElement.scrollTop = 950;
     });
 
     
   
     return (
-        <div className="schedulePageContainer">
-            <div className="headerContainer">
-                <a className="returnHome" href="/">
+        <div className="schedule-page-container">
+            <div className="header-container">
+                <a className="return-home" href="/">
                     <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="Black" className="bi bi-arrow-left" viewBox="0 0 20 5">
                         <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"></path>
                     </svg>
                 </a>
                 <p className="header-txt">Class Schedule - {headerText}</p>
             </div>
-            <div className="scheduleContainer">
+            <div className="schedule-container">
                 {generateGrids()}
-                <div className="test">
-                    Hola
-                </div>
+                {addOneSection("Tuesday", "2022-11-04T01:55:00.000+00:00", "PT2H30M", "Lecture")}
+                {addOneSection("Sunday", "2022-11-04T05:25:00.000+00:00", "PT1H15M", "Recitation")}
+                {addOneSection("Saturday", "2022-11-04T15:30:00.000+00:00", "PT50M", "Laboratory")}
             </div>
         </div>
     );
