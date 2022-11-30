717aedc994efe08cb0fbe0abaac4fa1a4b838d46
alexkobus <70383566+alexkobus@users.noreply.github.com>
2022-11-30 18:57:16 -0500

    add stats button to homepage

--- frontend/purdue-course-finder/src/components/Map.js
+++ frontend/purdue-course-finder/src/components/Map.js
@@ -383,6 +383,11 @@ function Map(props) {
             </div>
 
             <div className={classes.lowerFABdiv}>
+            <Fab className={classes.homeFAB} href='/stats'>
+                <SvgIcon>
+                  <path d="M10 10.02h5V21h-5zM17 21h3c1.1 0 2-.9 2-2v-9h-5v11zm3-18H5c-1.1 0-2 .9-2 2v3h19V5c0-1.1-.9-2-2-2zM3 19c0 1.1.9 2 2 2h3V10H3v9z" />
+                </SvgIcon>
+              </Fab>
               <Fab className={classes.homeFAB} href='/suggestions'>
                 <SvgIcon>
                   <path d="M9 21c0 .5.4 1 1 1h4c.6 0 1-.5 1-1v-1H9v1zm3-19C8.1 2 5 5.1 5 9c0 2.4 1.2 4.5 3 5.7V17c0 .5.4 1 1 1h6c.6 0 1-.5 1-1v-2.3c1.8-1.3 3-3.4 3-5.7 0-3.9-3.1-7-7-7z" />
